8a1902763d9c26c0a5c6608d9d8f5bcd7f9ddde3
Craig Fast <craigmaland@gmail.com>
2024-07-30 17:19:13 -0700

    added some necessary methods to build food cards

--- lib/main.dart
+++ lib/main.dart
@@ -77,26 +77,39 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
           ],
         ),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-        ),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: TabBarView(
+        controller: _tabController,
+        children: <Widget>[
+          SingleChildScrollView(
+            child: Column(
+                children: [
+                  ...List.generate(
+                    foodList.length,
+                        (index) => buildAllFoodCards(index),
+                  ),
+                  addNewFoodCard(),
+                ]
+            ),
+          ),
+          SingleChildScrollView(
+            child: Column(
+                children: [
+                  Text('Todo: add workout section')
+                ]
+            ),
+          ),
+        ],
+      ),
     );
+  }
+
+  //todo: add card info
+  Widget buildAllFoodCards(int index) {
+    return Card();
+  }
+
+  //todo: add card info
+  Widget addNewFoodCard() {
+    return Card();
   }
 }
