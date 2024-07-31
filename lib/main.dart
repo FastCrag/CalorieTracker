b3bc1d0e56de39cf8d8346b528c7f41e2db1c7e8
Craig Fast <craigmaland@gmail.com>
2024-07-30 23:11:26 -0700

    Added Functionality to add, delete new food/workouts.

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:calorie_tracker/food.dart';
 import 'package:calorie_tracker/workout.dart';
+import 'package:calorie_tracker/addFoodPage.dart';
+import 'package:calorie_tracker/addWorkoutPage.dart';
+import 'package:calorie_tracker/editFoodPage.dart';
+import 'package:calorie_tracker/editWorkoutPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -85,7 +89,7 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
                 children: [
                   ...List.generate(
                     foodList.length,
-                        (index) => buildAllFoodCards(index),
+                        (index) => createFoodCard(index),
                   ),
                   addNewFoodCard(),
                 ]
@@ -94,22 +98,254 @@ class _HomeState extends State<Home> with TickerProviderStateMixin {
           SingleChildScrollView(
             child: Column(
                 children: [
-                  Text('Todo: add workout section')
+                  ...List.generate(
+                    workoutList.length,
+                        (index) => createWorkoutCard(index),
+                  ),
+                  addNewWorkoutCard(),
                 ]
             ),
           ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        heroTag: "0",
+        onPressed: (){
+          if (_tabController.index == 0){_addFood();}
+          else {_addWorkout();}
+        },
+        tooltip: (_tabController.index == 0 ? "Add New Food":"Add New Workout"),
+        child: const Icon(Icons.add_circle_outline),
+      ),
     );
   }
 
-  //todo: add card info
-  Widget buildAllFoodCards(int index) {
-    return Card();
+  void _addFood() async {
+    // Use the Future returned by Navigator.push
+    var result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => AddFoodPage(
+          foodList: foodList,
+        ),
+      ),
+    );
+    if (result != null) {
+      setState(() {
+        build;
+      });
+    }
+  }
+
+  void _addWorkout() async {
+    // Use the Future returned by Navigator.push
+    var result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => AddWorkoutPage(
+          workoutList: workoutList,
+        ),
+      ),
+    );
+    if (result != null) {
+      setState(() {
+        build;
+      });
+    }
+  }
+
+  void _editFood(index) async {
+    var result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => EditFoodPage(
+          foodList: foodList,
+          index: index,
+        ),
+      ),
+    );
+    if (result != null) {
+      setState(() {
+        build;
+      });
+    }
+  }
+
+  void _editWorkout(index) async {
+    var result = await Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => EditWorkoutPage(
+          workoutList: workoutList,
+          index: index,
+        ),
+      ),
+    );
+    if (result != null) {
+      setState(() {
+        build;
+      });
+    }
   }
 
   //todo: add card info
+  Widget createFoodCard(int index) {
+    return Card(
+      elevation: 5,
+      clipBehavior: Clip.hardEdge,
+      child: InkWell(
+        splashColor: Colors.deepPurpleAccent,
+        onTap: () {
+          _editFood(index);
+        },
+        child: SizedBox(
+          width: 350,
+          child: SingleChildScrollView(
+            physics: ClampingScrollPhysics(),
+            scrollDirection: Axis.vertical,
+            child: Container(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                  mainAxisSize: MainAxisSize.min, // Set mainAxisSize to min
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            foodList[index].getName.toUpperCase(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                            ),
+                          ),
+
+                        ]
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Calories: ' + foodList[index].getCalories.toString(),
+                        ),
+                      ],
+                    ),
+                  ], //children
+              ),
+            ),
+          )
+        )
+
+      ),
+    );
+  }
+
+  Widget createWorkoutCard(int index) {
+    return Card(
+      elevation: 5,
+      clipBehavior: Clip.hardEdge,
+      child: InkWell(
+        splashColor: Colors.deepPurpleAccent,
+        onTap: () {
+          _editWorkout(index);
+        },
+        child: SizedBox(
+          width: 350,
+          child: SingleChildScrollView(
+            physics: ClampingScrollPhysics(),
+            scrollDirection: Axis.vertical,
+            child: Container(
+              padding: const EdgeInsets.all(15.0),
+              child: Column(
+                  mainAxisSize: MainAxisSize.min, // Set mainAxisSize to min
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            workoutList[index].getType.toUpperCase(),
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 25,
+                            ),
+                          ),
+
+                        ]
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'Calories: ' + workoutList[index].getCaloriesBurned.toString(),
+                        ),
+                      ],
+                    ),
+                  ], //children
+              ),
+            ),
+          )
+        )
+
+      ),
+    );
+  }
+
   Widget addNewFoodCard() {
-    return Card();
+    return Card(
+      elevation: 5,
+      clipBehavior: Clip.hardEdge,
+      child: InkWell(
+        splashColor: Colors.deepPurpleAccent,
+        onTap: () {
+          _addFood();
+        },
+        child: SizedBox(
+          width: 350,
+          height: 75,
+          child: Center(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Row(
+                    children: <Widget>[
+                      Icon(Icons.add_circle_outline),
+                      Center(child: Text(' Add a New Food ')),
+                    ]
+                ),
+              )
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget addNewWorkoutCard() {
+    return Card(
+      elevation: 5,
+      clipBehavior: Clip.hardEdge,
+      child: InkWell(
+        splashColor: Colors.deepPurpleAccent,
+        onTap: () {
+          _addWorkout();
+        },
+        child: SizedBox(
+          width: 350,
+          height: 75,
+          child: Center(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 15.0),
+                child: Row(
+                    children: <Widget>[
+                      Icon(Icons.add_circle_outline),
+                      Center(child: Text(' Add a New Workout ')),
+                    ]
+                ),
+              )
+          ),
+        ),
+      ),
+    );
   }
 }
